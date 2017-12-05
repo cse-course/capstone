6b9bda17e6b806a9bb31db7574866401eb529882
Hai Nguyen <1533478@hcmut.edu.vn>
2017-12-05 19:23:49 +0700

    update

--- app/src/main/java/com/github/nduyhai/capstone/MainActivity.java
+++ app/src/main/java/com/github/nduyhai/capstone/MainActivity.java
@@ -31,37 +31,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSolveQuadraticEquation(View view) {
+        try {
+            Double a = Double.valueOf(txtA.getText().toString());
+            Double b = Double.valueOf(txtB.getText().toString());
+            Double c = Double.valueOf(txtC.getText().toString());
 
-        Double a = Double.valueOf(txtA.getText().toString());
-        Double b = Double.valueOf(txtB.getText().toString());
-        Double c = Double.valueOf(txtC.getText().toString());
 
-        if (a == 0 && b == 0) {
-            Toast.makeText(this, "Have no real roots", Toast.LENGTH_LONG).show();
-        } else if (a == 0) {
-            Double x = -c / b;
-            Toast.makeText(this,
-                    String.join("\n", "Have exactly one real root", "x=" + x),
-                    Toast.LENGTH_LONG).show();
-        } else {
-            Double delta = b * b - 4 * a * c;
-            if (delta > 0) {  //2 root
-                Double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-                Double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-                ;
-                Toast.makeText(this,
-                        String.join("\n", "Have two distinct real roots", "x1=" + x1, "x2=" + x2),
-                        Toast.LENGTH_LONG).show();
-            } else if (delta == 0) { //1 root
-                Double x = -b / (2 * a);
+            if (a == 0 && b == 0) {
+                Toast.makeText(this, "Have no real roots", Toast.LENGTH_LONG).show();
+            } else if (a == 0) {
+                Double x = -c / b;
                 Toast.makeText(this,
                         String.join("\n", "Have exactly one real root", "x=" + x),
                         Toast.LENGTH_LONG).show();
-            } else { //0 root
-                Toast.makeText(this, "Have no real roots", Toast.LENGTH_LONG).show();
-            }
-        }
+            } else {
+                Double delta = b * b  - 4 * a * c;
 
+                if (delta > 0) {  //2 root
+                    Double x1 = (-b + Math.sqrt(delta))/(2*a);
+                    Double x2 = (-b - Math.sqrt(delta))/(2*a);;
+                    Toast.makeText(this,
+                            String.join("\n", "Have two distinct real roots", "x1=" + x1, "x2=" + x2),
+                            Toast.LENGTH_LONG).show();
+                } else if (delta == 0) { //1 root
+                    Double x = -b / (2 * a);
+                    Toast.makeText(this,
+                            String.join("\n", "Have exactly one real root", "x=" + x),
+                            Toast.LENGTH_LONG).show();
+                } else { //0 root
+                    Toast.makeText(this, "Have no real roots", Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception ex) {
+            Log.v("Invalid Number", ex.toString());
+            Toast.makeText(this, "Invalid number", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 }
