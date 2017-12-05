8e4a6caab05ebc86edc59b3dbe45c9dd5c8de1d8
Hai Nguyen <1533478@hcmut.edu.vn>
2017-12-05 19:48:48 +0700

    F

--- app/src/main/java/com/github/nduyhai/capstone/MainActivity.java
+++ app/src/main/java/com/github/nduyhai/capstone/MainActivity.java
@@ -1,6 +1,5 @@
 package com.github.nduyhai.capstone;
 
-import android.gesture.OrientedBoundingBox;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
